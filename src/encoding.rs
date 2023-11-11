08621c4c9a9be2ca5fe110b6bbc76fa0cc37e38b
Muneeb <muneebk015@gmail.com>
2023-11-11 23:35:05 +0530

    refactored encode

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,8 +1,32 @@
 use nannou::prelude::{pt2, Draw, Rect, STEELBLUE};
+
 pub trait Encoding {
     fn encode(&self, window: &Rect, binary_stream: &String, draw: &Draw);
 }
 
+fn encode_helper(window: &Rect, encoded_data: &Vec<usize>, draw: &Draw) {
+    let width = window.w();
+    let bit_length = width / encoded_data.len() as f32;
+    let mut previous_end = pt2(window.left(), -0.0);
+    for (i, &encoded_bit) in encoded_data.iter().enumerate() {
+        let height = if encoded_bit == 0 { -50.0 } else { 50.0 };
+        let start = pt2(window.left() + bit_length * i as f32, height);
+        let end = pt2(window.left() + bit_length * (i + 1) as f32, height);
+        if previous_end != start {
+            draw.line()
+                .start(previous_end)
+                .end(start)
+                .weight(4.0)
+                .color(STEELBLUE);
+        }
+        previous_end = end;
+        draw.line()
+            .start(start)
+            .end(end)
+            .weight(4.0)
+            .color(STEELBLUE);
+    }
+}
 pub trait Scrambling {
     fn scramble(&self, binary_stream: &String) -> String {
         binary_stream.to_string()
@@ -43,34 +67,13 @@ fn nrzi_encoder(bin_data: &Vec<usize>) -> Vec<usize> {
 
 impl Encoding for NRZI {
     fn encode(&self, window: &Rect, binary_stream: &String, draw: &Draw) {
-        let width = window.w();
-        let mut previous_end = pt2(window.left(), -0.0);
         let binary_data: Vec<usize> = binary_stream
             .chars()
             .map(|c| c.to_digit(10).unwrap() as usize)
             .collect();
         let encoded_data = nrzi_encoder(&binary_data);
-        let bit_length = width / encoded_data.len() as f32;
 
-        for (i, &encoded_bit) in encoded_data.iter().enumerate() {
-            let height = if encoded_bit == 0 { -50.0 } else { 50.0 };
-            let start = pt2(window.left() + bit_length * i as f32, height);
-            let end = pt2(window.left() + bit_length * (i + 1) as f32, height);
-
-            if previous_end != start {
-                draw.line()
-                    .start(previous_end)
-                    .end(start)
-                    .weight(4.0)
-                    .color(STEELBLUE);
-            }
-            previous_end = end;
-            draw.line()
-                .start(start)
-                .end(end)
-                .weight(4.0)
-                .color(STEELBLUE);
-        }
+        encode_helper(window, &encoded_data, draw);
     }
 }
 
@@ -116,34 +119,12 @@ fn manchester_encoder(bin_data: &Vec<usize>) -> Vec<usize> {
 
 impl Encoding for Manchester {
     fn encode(&self, window: &Rect, binary_stream: &String, draw: &Draw) {
-        let width = window.w();
-        let mut previous_end = pt2(window.left(), -0.0);
-
         let binary_data: Vec<usize> = binary_stream
             .chars()
             .map(|c| c.to_digit(10).unwrap() as usize)
             .collect();
         let encoded_data = manchester_encoder(&binary_data);
-        let bit_length = width / encoded_data.len() as f32;
-        for (i, &encoded_bit) in encoded_data.iter().enumerate() {
-            let height = if encoded_bit == 0 { -50.0 } else { 50.0 };
-            let start = pt2(window.left() + bit_length * i as f32, height);
-            let end = pt2(window.left() + bit_length * (i + 1) as f32, height);
-
-            if previous_end != start {
-                draw.line()
-                    .start(previous_end)
-                    .end(start)
-                    .weight(4.0)
-                    .color(STEELBLUE);
-            }
-            previous_end = end;
-            draw.line()
-                .start(start)
-                .end(end)
-                .weight(4.0)
-                .color(STEELBLUE);
-        }
+        encode_helper(window, &encoded_data, draw);
     }
 }
 
@@ -167,35 +148,12 @@ fn manchester_differential_encoder(bin_data: &Vec<usize>) -> Vec<usize> {
 
 impl Encoding for ManchesterDifferential {
     fn encode(&self, window: &Rect, binary_stream: &String, draw: &Draw) {
-        let width = window.w();
-        let mut previous_end = pt2(window.left(), -0.0);
-
         let binary_data: Vec<usize> = binary_stream
             .chars()
             .map(|c| c.to_digit(10).unwrap() as usize)
             .collect();
         let encoded_data = manchester_differential_encoder(&binary_data);
-        let bit_length = width / encoded_data.len() as f32;
-
-        for (i, &encoded_bit) in encoded_data.iter().enumerate() {
-            let height = if encoded_bit == 0 { -50.0 } else { 50.0 };
-            let start = pt2(window.left() + bit_length * i as f32, height);
-            let end = pt2(window.left() + bit_length * (i + 1) as f32, height);
-
-            if previous_end != start {
-                draw.line()
-                    .start(previous_end)
-                    .end(start)
-                    .weight(4.0)
-                    .color(STEELBLUE);
-            }
-            previous_end = end;
-            draw.line()
-                .start(start)
-                .end(end)
-                .weight(4.0)
-                .color(STEELBLUE);
-        }
+        encode_helper(window, &encoded_data, draw);
     }
 }
 
